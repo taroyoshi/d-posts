effd5b41354c49328e3b76aee1ab054afcb21306
taroyoshi <jetworldcircus@gmail.com>
2019-02-08 05:38:17 +0000

    0208 last

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,21 @@
 class UsersController < ApplicationController
-def new
-    @user = User.new
-end
-
-def create
-    @user = User.new(user_params)
-    if @user.save
-        
-    else
-        render 'new'
+    def show # 追加
+        @user = User.find(params[:id])
     end
-end
+
+
+   def new
+    @user = User.new
+   end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to @user
+        else
+            render 'new'
+        end
+    end
 
 private
 #userキーが存在するか検証、するならparams[:user]のうち以上のカラムの値を受け取る
